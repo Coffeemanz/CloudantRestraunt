c6df9486216454a0de637937155d969530d3d2b3
Coffeemanz <sergeyzhyshkevich@gmail.com>
2017-10-04 13:48:28 +0300

    Added Controller Advice

---
+++ src/main/java/com/iba/Web/exceptionhandlers/DefaultExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.iba.Web.exceptionhandlers;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(DefaultExceptionHandler.class);
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception e) throws Exception {
+        logger.error(e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
